ccc434ada1226ed364de2cd0a18c7a61feeac935
Victor Morris <92633344+Victorrmorris@users.noreply.github.com>
2023-08-17 21:30:58 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,39 +1,33 @@
+import streamlit as st
+import matplotlib.pyplot as plt
 from pybliometrics.scopus import ScopusSearch, AuthorRetrieval
 
-# Define search queries for the three areas
-query_factors = 'social innovation AND higher education institutions AND factors'
-query_change = 'social innovation AND higher education institutions AND organizational change'
+# Define search query for the impact analysis
 query_impact = 'social innovation AND higher education institutions AND impact assessment'
 
-# Conduct Scopus searches for each area
-results_factors = ScopusSearch(query_factors)
-results_change = ScopusSearch(query_change)
+# Conduct Scopus search for impact analysis
 results_impact = ScopusSearch(query_impact)
 
 # Function to display publication information with type specification and geographical location
 def display_publications(results, publication_type):
-    print(f"{publication_type.capitalize()} Publications:")
+    st.subheader(f"{publication_type.capitalize()} Publications:")
     for i, result in enumerate(results.results, start=1):
-        authors = ', '.join(result.authors)
-        author_affiliations = []
+        # Display publication information
+        st.write(f"**{publication_type} {i}:**")
+        st.write("**Title:**", result.title)
+        st.write("**Authors:**", ', '.join(result.authors))
+        st.write("**Publication Year:**", result.coverDate)
+        st.write("**DOI:**", result.doi)
+        st.write("**Citations:**", result.citedby_count)
+        st.write("**Abstract:**", result.description)
+        st.write("\n")
+        
+        # Create a simple bar chart for citations
+        plt.bar(result.title, result.citedby_count)
+        plt.xticks(rotation=45, ha="right")
+        plt.ylabel("Citations")
+        st.pyplot(plt)
 
-        # Retrieve and concatenate author affiliations
-        for author_id in result.author_ids:
-            author = AuthorRetrieval(author_id)
-            author_affiliations.append(author.affiliation)
-
-        # Display publication information and author affiliations
-        print(f"{publication_type} {i}:")
-        print("Title:", result.title)
-        print("Authors:", authors)
-        print("Author Affiliations:", ', '.join(author_affiliations))
-        print("Publication Year:", result.coverDate)
-        print("DOI:", result.doi)
-        print("Citations:", result.citedby_count)
-        print("Abstract:", result.description)
-        print("\n")
-
-# Display information for each area with publication types specified
-display_publications(results_factors, "Book")
-display_publications(results_change, "Journal Article")
+# Streamlit app
+st.title("Bibliometric Analysis - Impact Analysis")
 display_publications(results_impact, "Report")
